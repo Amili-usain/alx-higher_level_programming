30c319be42a02d21ede7b22141a9151698852402
Amili-usain <amilihussein073@gmail.com>
2022-09-20 06:07:05 -0700

    task 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for n in range(1, 101):
+        if n % 3 == 0:
+            print("Fizz", end='')
+        if n % 5 == 0:
+            print("Buzz", end='')
+        if n % 3 and n % 5:
+            print("{:d}".format(n), end='')
+        print(end=' ')
