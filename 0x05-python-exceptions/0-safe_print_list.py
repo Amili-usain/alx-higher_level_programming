90807ae129c0a9b6f135cd1f29772ffa68b8c5a9
Amili-usain <amilihussein073@gmail.com>
2022-10-10 09:27:23 -0700

    fixed space around =

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     ret = 0
     for i in range(x):
-        try:i
+        try:
             print(my_list[i], end="")
             ret += 1
         except IndexError:
