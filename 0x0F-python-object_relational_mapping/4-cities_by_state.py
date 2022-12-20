9db7d5db01a2e7c034c507870087b56602851b32
Amili-usain <amilihussein073@gmail.com>
2022-12-20 09:28:50 -0800

    mandatory tasks 0-14

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This script lists all cities from the database `hbtn_0e_4_usa`."""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """Access to the database and getting the cities from the database."""
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    cur.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON cities.state_id = states.id;")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
