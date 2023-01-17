f5c03643ada2f8dcdb7bd91198d162baeea320cf
Amili-usain <amilihussein073@gmail.com>
2023-01-17 07:07:26 -0800

    mandatory tasks 0-9

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""Python script that fetches the URL: https://intranet.hbtn.io/status with
-`requests` module
 """
-
+Python script that fetches an URL with requests package
+"""
 import requests
 
 
 if __name__ == "__main__":
-    req = requests.get('https://intranet.hbtn.io/status')
-
-    print('Body response:')
-    print('\t- type: {_type}'.format(_type=type(req.text)))
-    print('\t- content: {_content}'.format(_content=req.text))
+    r = requests.get('https://intranet.hbtn.io/status')
+    t = r.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
